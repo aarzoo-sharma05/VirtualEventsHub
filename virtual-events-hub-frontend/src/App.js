df572c2452fdb82c7d46fca1c9f94593f06e4b34
Aarzoo Sharma <aarzoosharma@Aarzoos-MacBook-Pro.local>
2024-01-26 14:01:45 -0800

    Adding routes and updated gitignore to ignore unnecessary files

--- virtual-events-hub-frontend/src/App.js
+++ virtual-events-hub-frontend/src/App.js
@@ -1,43 +1,19 @@
-import logo from './logo.svg';
 // virtual-events-hub-frontend/src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EventList from './components/EventList';
 import EventDetail from './components/EventDetail';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route path="/" exact component={EventList} />
         <Route path="/event/:id" component={EventDetail} />
-      </Switch>
+      </Routes>
     </Router>
   );
 };
 
 export default App;
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
